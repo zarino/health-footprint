a3498568cc5f605b4258aea8fccd5858d7c2b3be
Zarino Zappia <mail@zarino.co.uk>
2012-12-16 17:56:55 +0000

    preparing php includes to work from nested directories

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require 'config.php';
-require 'functions.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/functions.php';
 
 connect_to_database();
 # setup_database();
-  
+
 $r = mysqli_query($GLOBALS['dbc'], "SELECT now()");
 while($row=mysqli_fetch_array($r)){
     pretty_print_r($row);
